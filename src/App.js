24c8bde501498191c3edb2b36758e8a3d39ac929
Banel Mgwevu <71422029+Banel-Mgwevu@users.noreply.github.com>
2024-01-15 06:23:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './Navbar';
 
 function App() {
+
+  const title = "Welcome to my blog";
+  const likes = 50;
+  const link = "https://www.google.com";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <NavBar/>
+      <div className='content'>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{110}</p>
+        <p>{"Hello"}</p>
+        <p>{[1, 2, 3]}</p>
+        <p>{Math.random()}</p>
+        <a href={link}>Google site</a>
+      </div>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const NavBar =()=>{
+return (
+    <nav className="navbar">
+        <h1>Blog Name</h1>
+        <div className="links">
+            <a href="/">Home</a>
+            <a href="/create">New blog</a>
+        </div>
+    </nav>
+);
+
+
+}
+export default NavBar;
